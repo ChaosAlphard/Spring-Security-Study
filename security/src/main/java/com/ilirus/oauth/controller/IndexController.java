d67167ac0a63990b867725797a2bc94753103185
ChaosAlphard <Alphard450@outlook.com>
2020-04-01 04:30:06 +0800

    🎨修改页面样式

--- security/src/main/java/com/ilirus/oauth/controller/IndexController.java
+++ security/src/main/java/com/ilirus/oauth/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.ilirus.oauth.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +13,13 @@ public class IndexController {
     }
 
     @RequestMapping("/login")
-    public String login() {
+    public String login(String error, String logout, Model model) {
+        if(error != null) {
+            model.addAttribute("err", "认证失败");
+        }
+        if(logout != null) {
+            model.addAttribute("logout", "你已注销");
+        }
         return "login";
     }
 
