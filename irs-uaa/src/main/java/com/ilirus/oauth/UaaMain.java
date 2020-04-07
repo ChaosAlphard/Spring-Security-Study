e2d0528fd198942599b2d4a8f06605e4642f680a
ChaosAlphard <Alphard450@outlook.com>
2020-04-07 10:44:47 +0800

    ✨OAuth

---
+++ irs-uaa/src/main/java/com/ilirus/oauth/UaaMain.java
@@ -0,0 +1,11 @@
+package com.ilirus.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaaMain {
+    public static void main(String[] args) {
+        SpringApplication.run(UaaMain.class, args);
+    }
+}
