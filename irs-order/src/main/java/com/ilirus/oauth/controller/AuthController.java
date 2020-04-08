e6ec72f8ebac8e89420195dae5d8223ff9e7c092
ChaosAlphard <Alphard450@outlook.com>
2020-04-09 00:51:35 +0800

    🚧授权认证控制器

---
+++ irs-order/src/main/java/com/ilirus/oauth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.ilirus.oauth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthController {
+    @RequestMapping("/auth")
+    public String authorize() {
+        return "oauth/authorize";
+    }
+
+    @RequestMapping("/token")
+    public String token(String code, String error, String error_description, Model model) {
+        if(code != null) {
+            model.addAttribute("code", code);
+        }
+        if(error != null) {
+            model.addAttribute("error", "授权失败");
+        }
+        if(error_description != null) {
+            model.addAttribute("error_description", error_description);
+        }
+        return "oauth/token";
+    }
+}
